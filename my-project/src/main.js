001693dea766583d15dd2c2539a9c33799b3f877
ryanvann33 <rvann377@gmail.com>
2023-06-06 22:20:48 -0400

    Update main.js

--- my-project/src/main.js
+++ my-project/src/main.js
@@ -9,4 +9,6 @@ const supabaseUrl = 'https://zpzggknvmyqoiufanaqn.supabase.co'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inpwemdna252bXlxb2l1ZmFuYXFuIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODU0OTY0NjIsImV4cCI6MjAwMTA3MjQ2Mn0.FPNbO_mB_JOPEQ_uKAOFbnoBdrYngB-NmBbiN2fZl0A'
 const supabase = createClient(supabaseUrl, supabaseKey)
 
-createApp(App).provide('supabase', supabase).mount('#app')
+const app = createApp(App)
+app.config.globalProperties.$supabase = supabase
+app.mount('#app')
